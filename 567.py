b247c9d2e3898861827a7c2d4806e0caa4a84c58
Zachary Bohn <zacharybohn95@gmail.com>
2025-01-09 15:39:44 -0500

    567

---
+++ 567.py
@@ -0,0 +1,36 @@
+from string import ascii_lowercase
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1_counts = {}
+        window_counts = {}
+        for x in ascii_lowercase:
+            s1_counts[x] = 0
+            window_counts[x] = 0
+        for x in s1:
+            s1_counts[x] += 1
+        # doing len(s1)-1 here might be a bit weird?
+        # keeps the code farther down more concise tho
+        for x in s2[:len(s1)-1]:
+            window_counts[x] += 1
+        # main iteration
+        # both pointers are inclusive
+        left = 0
+        for right in range(len(s1) - 1, len(s2)):
+            window_counts[s2[right]] += 1
+            if self.isEqual(s1_counts, window_counts):
+                return True
+            window_counts[s2[left]] -= 1
+            left += 1
+        return False
+
+    def isEqual(self, s1_counts, window_counts):
+        if len(s1_counts) != len(window_counts):
+            return False
+        for x in s1_counts:
+            if s1_counts[x] != window_counts[x]:
+                return False
+        return True
