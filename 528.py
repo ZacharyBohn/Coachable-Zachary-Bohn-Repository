59a0effe8e36026207fc23bbf5dc6168c776c548
Zachary Bohn <zacharybohn95@gmail.com>
2025-04-16 11:34:37 -0400

    Follows inclusive-exclusive convention

--- 528.py
+++ 528.py
@@ -8,21 +8,21 @@ class Solution:
         self.total = 0
         self.ranges = []
         for x in w:
-            # [inclusive, inclusive] range
-            self.ranges.append([self.total + 1, self.total+x])
+            # [inclusive, exclusive] range
+            self.ranges.append([self.total, self.total+x])
             self.total += x
 
     def pickIndex(self) -> int:
-        seed = random.randint(1, self.total)
+        seed = random.randint(0, self.total - 1)
         left, right = 0, len(self.ranges) - 1
         while left <= right:
             mid = left + ((right - left) // 2)
-            if seed >= self.ranges[mid][0] and seed <= self.ranges[mid][1]:
+            if seed >= self.ranges[mid][0] and seed < self.ranges[mid][1]:
                 # target found
                 return mid
             elif seed < self.ranges[mid][0]:
                 right = mid - 1
-            elif seed > self.ranges[mid][1]:
+            elif seed >= self.ranges[mid][1]:
                 left = mid + 1
             else:
                 raise Exception('error in binary search')
@@ -32,3 +32,11 @@ class Solution:
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
+
+'''
+3, 5 ->
+
+[0, 3] = 3 - 0 = length of 3
+   [3, 8] = 8 - 3 = length of 5
+
+'''
